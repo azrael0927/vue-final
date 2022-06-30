00b42b49b05d3a05f3b1dff32e0694ab680b411e
李文鵬 <a10461321@gmail.com>
2022-06-30 23:55:08 +0800

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <nav>
+    <div class="btn btn-primary">55555</div>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
@@ -7,24 +8,5 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import './assets/all';
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import VueLoading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+app.component('VueLoading', VueLoading);
+app.use(VueAxios, axios);
+app.use(router);
+app.mount('#app');
